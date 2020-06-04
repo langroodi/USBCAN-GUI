5167a86a21503013e04d513a6fd99284cacac1e4
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2020-06-04 21:59:44 +0200

    COM Port Group Box is added.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,29 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSerialPort>
+#include <QSerialPortInfo>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    updateSerialPorts();
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    updateSerialPorts();
+}
+
+void MainWindow::updateSerialPorts()
+{
+    ui->comboBox->clear();
+    QList<QSerialPortInfo> comPorts = QSerialPortInfo::availablePorts();
+
+    foreach (QSerialPortInfo comPort, comPorts) {
+        ui->comboBox->addItem(comPort.systemLocation());
+    }
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    void updateSerialPorts();
 };
 
 #endif // MAINWINDOW_H
